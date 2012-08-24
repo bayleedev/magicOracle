dd82dcac9a01b6667647d851c13418ffcd667c28
Blaine Schmeisser <blaine.schmeisser@vitals.com>
2012-08-23 20:51:37 -0500

    Update bootstrap to include mock files.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,9 @@
 <?php
-require_once(__DIR__ . '/src/Oracle.php');
+// Get source
+require_once(__DIR__ . '/../src/Oracle.php');
+
+// Get mocks
+$mocks = array_slice(scandir(__DIR__ . '/mocks'), 2);
+foreach($mocks as $mock) {
+	require_once(__DIR__ . '/mocks/' . $mock);
+}
---
+++ tests/mocks/Foo.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace magicOracle\tests\mocks;
+
+class Foo {
+	public $publicProperty;
+	private $privateProperty;
+	protected $protectedProperty;
+
+	public function __construct($args) {
+		foreach($args as $key => $value) {
+			$this->key = $value;
+		}
+	}
+
+	public function public_strtolower($words) {
+		return strtolower($words);
+	}
+
+	private function private_ucwords($words) {
+		return ucwords($words);
+	}
+
+	protected function protected_strtouper($words) {
+		return strtoupper($words);
+	}
+}
